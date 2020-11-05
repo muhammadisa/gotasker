7ba9c3cd4f0fda1b5daac01395626bdc82f15851
muhammadisa <msawjk@gmail.com>
2020-11-05 17:57:26 +0700

    Refactoring

--- cron/app/deadline/action.go
+++ cron/app/deadline/action.go
@@ -7,49 +7,50 @@ import (
 	"time"
 )
 
+func currentTimeAddByString(days int) string {
+	return time.Date(
+		2020, 11, 16,
+		0, 0, 0, 0, time.Local).
+		AddDate(0, 0, days).
+		Format("2006-01-02")
+}
+
+func retrieveDaysRemaining(sess *dbr.Session) []int {
+	var days []int
+	_, err := sess.Select("days_remaining").
+		From("deadlines").
+		Load(&days)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	return days
+}
+
+func retrieveDeadlines(sess *dbr.Session, day int, deadlinesFiltered *[]models.Deadline) {
+	_, err := sess.Select("*").
+		From("deadlines").
+		Where("deadline = ? AND warned = ?", currentTimeAddByString(day), false).
+		Load(deadlinesFiltered)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func extractDeadlines(sess *dbr.Session, daysRemaining []int) []models.Deadline {
+	var deadlinesFiltered []models.Deadline
+	for i := 1; i <= len(daysRemaining)-1; i++ {
+		retrieveDeadlines(sess, daysRemaining[i], &deadlinesFiltered)
+	}
+	return deadlinesFiltered
+}
+
 // DoAction which related to deadline cron
 func DoAction(sess *dbr.Session) {
 	defer log.Println("execute completed")
-	formatLayout := "2006-01-02"
 
-	var err error
-	var daysRemaining []int
-	var deadlinesFiltered []models.Deadline
-
-	//Date(2020, 11, 05, 0, 0, 0, 0, time.Local)
-	//Add(time.Hour * 24 * 1 * time.Duration(-deadline.DaysRemaining))
-	//currentTime := time.
-	//	Now().Add(time.Hour * 24 * 1 * time.Duration(3)).Format(formatLayout)
-	//log.Println(currentTime)
-
-	// Retrieve all deadlines
-	_, err = sess.Select("days_remaining").
-		From("deadlines").
-		Load(&daysRemaining)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Scanning all deadlines
-	for indexDays, day := range daysRemaining {
-		// Add current time with days remaining
-		ctAddByDaysRemaining := time.
-			Date(2020, 11, 20, 0, 0, 0, 0, time.Local).
-			AddDate(0, 0, day).
-			Format(formatLayout)
-		if indexDays != len(daysRemaining)-1 {
-			// retrieve deadline wants tobe warned
-			_, err = sess.Select("*").
-				From("deadlines").
-				Where("deadline = ? AND warned = ?", ctAddByDaysRemaining, false).
-				Load(&deadlinesFiltered)
-			if err != nil {
-				log.Println(err)
-			}
-		}else{
-			break
-		}
-	}
+	daysRemaining := retrieveDaysRemaining(sess)
+	deadlinesFiltered := extractDeadlines(sess, daysRemaining)
 
 	for index, deadline := range deadlinesFiltered {
 		log.Println("Publish Message", index, deadline)
